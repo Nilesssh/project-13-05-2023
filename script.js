ee9d90954b63546155f45882e721145685ea2a88
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-13 13:11:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,37 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  console.log(arr.map(element => element.marks>50));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(element => {
+    if(element.marks>50){
+      console.log(element);
+    }
+  });
+
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45})
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  console.log(arr.filter(element => element.marks>50));
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr1 = [
+    { id: 5, name: "vishal", age: "23", marks: 70 },
+    { id: 6, name: "keshav", age: "12", marks: 55 },
+    { id: 7, name: "narayan", age: "16", marks: 39 },
+  ];
+  console.log([...arr,...arr1]);
 }
